d8edfba664c01b9ec1db5560102035de0aad3ea1
mreorhan <videokod7@gmail.com>
2018-05-12 16:42:09 +0300

    Changed file structure

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 module.exports=()=>{
-    mongoose.connect('mongodb://localhost:27017');
+    mongoose.connect('mongodb://emre:deneme123@ds117200.mlab.com:17200/deneme');
+    //mongoose.connect('mongodb://localhost:27017');
     mongoose.connection.on('open',()=>{
         console.log('Connection OK');
     })
