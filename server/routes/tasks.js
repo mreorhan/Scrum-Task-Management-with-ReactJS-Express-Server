99c22ca8976a5dc147d2aad4d854f354962e6953
Emre Orhan <videokod7@gmail.com>
2018-09-12 13:09:10 +0300

    Added one task model

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -16,7 +16,7 @@ router.post('/',(req,res,next)=>{
 })
 
 //Hangi statusta kaç tane task olduğunu gösterir
-router.get('/count',(req,res)=>{
+router.get('/counter',(req,res)=>{
   const promise = Task.aggregate([
     {
       $group:{
@@ -39,6 +39,64 @@ router.get('/:id',(req,res)=>{
     {
       $match:{
         storyId:  parseInt(req.params.id)
+      }
+    },
+    {
+      $lookup:{
+        from:'users',
+        localField:'contributors',
+        foreignField:'_id',
+        as:'contributors'
+      }
+    },
+    {
+      $unwind:{
+        path:'$contributors'
+      }
+    },
+    {
+      $group:{
+        _id:{
+          _id:'$_id',
+          content:'$content',
+          title:'$title',
+          status:'$status',
+          date:'$date',
+          color:'$color',
+          dueDate:'$dueDate',
+          createdBy:'$createdBy'
+        },
+        contributors:{
+        $push:'$contributors'
+    }
+  }
+  },
+    {
+      $project:{
+        _id:'$_id._id',
+        content:'$_id.content',
+        title:'$_id.title',
+        status:'$_id.status',
+        date:'$_id.date',
+        dueDate:'$_id.dueDate',
+        color:'$_id.color',
+        createdBy: '$_id.createdBy',
+        contributors: '$contributors',
+      }
+    }
+  ]);
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+})
+//tek task yazar
+router.get('/task/:id',(req,res)=>{
+  const promise = Task.aggregate([
+    {
+      $match:{
+        _id:  parseInt(req.params.id)
       }
     },
     {
